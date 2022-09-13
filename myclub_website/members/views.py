d395eb54681a8f65532f0ee84191fba8ca2c0ad4
Jash7447 <jashparikh7447@gmail.com>
2022-09-13 22:41:45 +0530

    Initial commit

---
+++ myclub_website/members/views.py
@@ -0,0 +1,29 @@
+import re
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
+			return redirect('login')	
+	else:
+		return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Were Sucessfully logged out!!"))	
+	return redirect('home')
+
+def register_user(request):
+	return render(request, 'authenticate/register_user.html', {})
+	
